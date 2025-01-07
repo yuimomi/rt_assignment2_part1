50a7b779804a501805b8f816017641140ff1db51
yuimomi <yuimomi0513@gmail.com>
2025-01-07 09:40:55 +0000

    FIX action client

--- src/target_service_node.cpp
+++ src/target_service_node.cpp
@@ -4,16 +4,12 @@
 double last_x = 0.0;
 double last_y = 0.0;
 
-// UKn¹ÕgSnÎüÉæü¶e¢¯·çó¯é¤¢óÈK
-// ¿ü²ÃÈ×QÖcfô°YÅLB
-// SSgoîkÈÔÃ¯ü­g×QÖ]n$°íüÐë	pkÝXYhY
 void targetCallback(const geometry_msgs::Point::ConstPtr& msg)
 {
   last_x = msg->x;
   last_y = msg->y;
 }
 
-// µüÓ¹³üëÐÃ¯
 bool getLastTarget(my_assignment2::LastTarget::Request &req,
                    my_assignment2::LastTarget::Response &res)
 {
@@ -28,10 +24,8 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "target_service_node");
   ros::NodeHandle nh;
 
-  // µüÓ¹µüÐ\
   ros::ServiceServer service = nh.advertiseService("get_last_target", getLastTarget);
 
-  // SSgo¿ü²ÃÈô°(kÈÔÃ¯ü­
   ros::Subscriber sub = nh.subscribe("/last_target_topic", 10, targetCallback);
 
   ros::spin();
